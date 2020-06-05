ef7d4262a501f2ee9958eb9a34ec7bfcbff54c20
vish <shakayavishal007@gmail.com>
2020-06-05 13:35:19 +0530

    adding custom model

--- Rest_project/settings.py
+++ Rest_project/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+temp_dir = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Rest_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [temp_dir,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Rest_project/urls.py
+++ Rest_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from Rest import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.Home.as_view(),name='home'),
     path('api-auth/', include('rest_framework.urls')),
+
 ]
